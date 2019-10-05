d59219a6c35ac30d55450a27e0d52cf70c37c1c9
diegolt8 <diegoalugo@hotmail.com>
2019-10-05 00:32:46 -0500

    [resolves #7] resolve indent

--- app/repositories/TipoPracticaRepository.js
+++ app/repositories/TipoPracticaRepository.js
@@ -2,5 +2,4 @@ const TipoPracticaRepository = module.exports;
 const DB = require('../utils/DB');
 
 TipoPracticaRepository.create = tipoPractica => DB('tipo_practica').insert(tipoPractica).returning('*');
-
-TipoPracticaRepository.find =  id => DB('tipo_practica').select('*').where({ id }).first();
+TipoPracticaRepository.find = id => DB('tipo_practica').select('*').where({ id }).first();
